65dc6b074f53ebc37bd97be18cbe3c85769a2613
Keegan Neave <keegan@neave.engineering>
2023-03-20 16:10:06 +0000

    bulk read both arms, calculate angles for each

---
+++ python/src/nefive/jointlist.py
@@ -0,0 +1,18 @@
+servos = {
+    "LEFT_SHOULDER_INOUT":       100,
+    "LEFT_SHOULDER_FOREAFT":     101,
+    "LEFT_UPPERARM_ROTATE":      102,
+    "LEFT_ELBOW":                103,
+    "LEFT_WRIST_ROTATE":         104,
+    "LEFT_WRIST_LEFTRIGHT":      105,
+    "LEFT_WRIST_UPDOWN":         106,
+    "LEFT_HAND":                 107,
+    "RIGHT_SHOULDER_INOUT":      108,
+    "RIGHT_SHOULDER_FOREAFT":    109,
+    "RIGHT_UPPERARM_ROTATE":     110,
+    "RIGHT_ELBOW":               111,
+    "RIGHT_WRIST_ROTATE":        112,
+    "RIGHT_WRIST_LEFTRIGHT":     113,
+    "RIGHT_WRIST_UPDOWN":        114,
+    "RIGHT_HAND":                115
+}
