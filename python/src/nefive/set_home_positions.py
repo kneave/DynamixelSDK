b9da0d71b73c0efc79d646264464dca4eb4ff73b
Keegan Neave <keegan@neave.engineering>
2023-03-26 16:44:36 +0100

    Create set_home_positions.py

---
+++ python/src/nefive/set_home_positions.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Copyright 2017 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+#*******************************************************************************
+#***********************     Bulk Read and Bulk Write Example      ***********************
+#  Required Environment to run this example :
+#    - Protocol 2.0 supported DYNAMIXEL(X, P, PRO/PRO(A), MX 2.0 series). Note that the XL320 does not support Bulk Read and Bulk Write. 
+#    - DYNAMIXEL Starter Set (U2D2, U2D2 PHB, 12V SMPS)
+#  How to use the example :
+#    - Select the DYNAMIXEL in use at the MY_DXL in the example code. 
+#    - Build and Run from proper architecture subdirectory.
+#    - For ARM based SBCs such as Raspberry Pi, use linux_sbc subdirectory to build and run.
+#    - https://emanual.robotis.com/docs/en/software/dynamixel/dynamixel_sdk/overview/
+#  Author: Ryu Woon Jung (Leon)
+#  Maintainer : Zerom, Will Son
+# *******************************************************************************
+
+import os
+from jointlist import servos
+import math
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+from dynamixel_sdk import *                    # Uses Dynamixel SDK library
+
+
+#********* DYNAMIXEL Model definition *********
+#***** (Use only one definition at a time) *****
+MY_DXL = 'X_SERIES'       # X330 (5.0 V recommended), X430, X540, 2X430
+
+# Control table address
+if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
+    ADDR_TORQUE_ENABLE          = 64
+    ADDR_LED_RED                = 65
+    LEN_LED_RED                 = 1         # Data Byte Length
+    ADDR_GOAL_POSITION          = 116
+    LEN_GOAL_POSITION           = 4         # Data Byte Length
+    ADDR_PRESENT_POSITION       = 132
+    LEN_PRESENT_POSITION        = 4         # Data Byte Length
+    BAUDRATE                    = 1000000
+
+# DYNAMIXEL Protocol Version (1.0 / 2.0)
+# https://emanual.robotis.com/docs/en/dxl/protocol2/
+PROTOCOL_VERSION            = 2.0
+
+# Use the actual port assigned to the U2D2.
+# ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
+DEVICENAME                  = 'COM3'
+
+TORQUE_ENABLE               = 1                 # Value for enabling the torque
+TORQUE_DISABLE              = 0                 # Value for disabling the torque
+LED_ENABLE                  = 0x01              # Value for enabling the led
+LED_DISABLE                 = 0x00              # Value for disabling the led
+DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
+
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+# Initialize GroupBulkWrite instance
+groupBulkWrite = GroupBulkWrite(portHandler, packetHandler)
+
+# Initialize GroupBulkRead instace for Present Position
+groupBulkRead = GroupBulkRead(portHandler, packetHandler)
+
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+def setLED(id, value):
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, id, ADDR_LED_RED, value)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print(f"[{id}] servo LED is now {value}")
+
+
+# Add parameter storage for Dynamixel#1 present position
+for key, value in servos.items():
+    dxl_addparam_result = groupBulkRead.addParam(value, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+    if dxl_addparam_result != True:
+        print("[ID:%03d] groupBulkRead addparam failed" % value)
+        quit()
+
+    # Disable LEDs
+    setLED(value, LED_DISABLE)
+
+    print("Press any key to continue! (or press ESC to quit!)")
+    if getch() == chr(0x1b):
+        break
+
+# Bulkread present position and LED status
+dxl_comm_result = groupBulkRead.txRxPacket()
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    exit()
+
+def getAngleFromPosition(id, position):
+    start_addr = groupBulkRead.data_dict[value][PARAM_NUM_ADDRESS]
+    current_position = bytes([
+        groupBulkRead.data_dict[value][PARAM_NUM_DATA][ADDR_PRESENT_POSITION - start_addr + 0],
+        groupBulkRead.data_dict[value][PARAM_NUM_DATA][ADDR_PRESENT_POSITION - start_addr + 1],
+        groupBulkRead.data_dict[value][PARAM_NUM_DATA][ADDR_PRESENT_POSITION - start_addr + 2],
+        groupBulkRead.data_dict[value][PARAM_NUM_DATA][ADDR_PRESENT_POSITION - start_addr + 3]])
+
+    position_signed = int.from_bytes(current_position, "little", signed=True)
+    current_angle = position_signed * 0.088
+    current_angle = round(current_angle, 2)
+
+angles = {}
+for key, value in servos.items():
+    # Check if groupbulkread data of Dynamixel#1 is available
+    dxl_getdata_result = groupBulkRead.isAvailable(value, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+    if dxl_getdata_result != True:
+        print("[ID:%03d] groupBulkRead getdata failed" % value)
+        quit()
+
+    current_angle = getAngleFromPosition(value, dxl_getdata_result)
+    angles.append(value, current_angle)
+
+
+
+# Clear bulkread parameter storage
+groupBulkRead.clearParam()
+
+# # Disable Dynamixel#1 Torque
+# dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+# if dxl_comm_result != COMM_SUCCESS:
+#     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+# elif dxl_error != 0:
+#     print("%s" % packetHandler.getRxPacketError(dxl_error))
+#
+# # Disable Dynamixel#2 Torque
+# dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL2_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+# if dxl_comm_result != COMM_SUCCESS:
+#     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+# elif dxl_error != 0:
+#     print("%s" % packetHandler.getRxPacketError(dxl_error))
+#
+# Close port
+portHandler.closePort()
